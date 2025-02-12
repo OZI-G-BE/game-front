a5213e95fea0f10aea2173ad70c513c4be75d876
Olorunjuwon Adewumi <freshguyxo@gmail.com>
2025-02-12 10:15:49 +0100

    feat: update Footer component with contact info and navigation links; enhance styles for better layout

--- Game-Site/src/Components/Footer.jsx
+++ Game-Site/src/Components/Footer.jsx
@@ -3,16 +3,25 @@ import styles from './Footer.module.css';
 function Footer(){
     return(
         <footer>
-            <div className="details">
-                <h3>Contact Info</h3>
-                p
-                <ul></ul>
-            </div>
-            <div className="details">
-                <h3>Navigation</h3>
-            </div>
-            <div className="details">
-                
+            <div className={styles.footerContainer}>
+                <div className={styles.details}>
+                    <h3 className={styles.footHead}>Contact Info</h3>
+                    <ul className={styles.footList}>
+                        <li>Email: Game-Info@gmail.com</li>
+                        <li>Phone: 08126210413</li>
+                        <li>Address: 1234 Game Street, Lagos, Nigeria</li>
+                    </ul>
+                </div>
+                <div className={styles.details}>
+                    <h3 className={styles.footHead}>Navigation</h3>
+                    <ul className={styles.footList}>
+                        <li><a href="">Home</a></li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className={styles.details}>
+                    <h1 className={styles.footnote}>FIGHT <br /> ON!</h1>
+                </div>
             </div>
         </footer>
     );
